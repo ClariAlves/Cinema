6be6e3ab7f6290329fbbd1719280430c967b7bd8
Clarice Alves <119855428+ClariAlves@users.noreply.github.com>
2023-03-29 19:37:51 -0300

    Add files via upload

---
+++ CompDeCompra.java
@@ -0,0 +1,12 @@
+package cinema;
+
+public class CompDeCompra {
+
+	public static void main(String[] args) {
+		
+		Filme f1 = new Filme("Clarice Linda", "Fantasia", "'Em um lindo dia, uma garota resolve matar pessoas'", "www", 140, 18); 
+		f1.status();
+
+	}
+
+}
