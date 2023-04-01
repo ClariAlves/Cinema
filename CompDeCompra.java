d03c38caa7b42c8537ca54cd361183770ddc2422
Clarice Alves <119855428+ClariAlves@users.noreply.github.com>
2023-04-01 18:34:35 -0300

    Update CompDeCompra.java

--- CompDeCompra.java
+++ CompDeCompra.java
@@ -10,6 +10,8 @@ public class CompDeCompra {
 		Sessoes s1 = new Sessoes(5, "18:00", "Legendado");
 		
 		Datas dia = new Datas("Terça-Feira");
+		
+		Pagamento pag = new Pagamento(5056203158102570L, 897, "Compras", 2028, 71369852185L);
 	}
 
 }
