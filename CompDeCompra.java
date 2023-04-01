d75a2d50bcb2bb4fe47f9d231749c9f8302203ef
Clarice Alves <119855428+ClariAlves@users.noreply.github.com>
2023-04-01 13:01:03 -0300

    Update CompDeCompra.java

--- CompDeCompra.java
+++ CompDeCompra.java
@@ -6,7 +6,10 @@ public class CompDeCompra {
 		
 		Filme f1 = new Filme("Clarice Linda", "Fantasia", "'Em um lindo dia, uma garota resolve matar pessoas'", "www", 140, 18); 
 		f1.status();
-
+		
+		Sessoes s1 = new Sessoes(5, "18:00", "Legendado");
+		
+		Datas dia = new Datas("Terça-Feira");
 	}
 
 }
